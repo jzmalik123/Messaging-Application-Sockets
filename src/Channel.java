03ac231f268fc041a155082874bc09ca4613c565
Jahanzaib <jzmalik123@gmail.com>
2023-07-17 02:44:30 +0500

    Add methods for Channel

--- src/Channel.java
+++ src/Channel.java
@@ -1,50 +1,55 @@
 
+import org.json.simple.JSONObject;
+
 import java.util.ArrayList;
 
 
 // Channel class to store channels and allow users to publish messages and subcribe and unsubscribe it
 public class Channel {
-    private String channelId;
-    private String channelName;
-    private ArrayList<Message> messages;
-    private ArrayList<String> users;
-    public Channel(String channelId, String channelName, ArrayList<String> users) {
-        this.channelId = channelId;
-        this.channelName = channelName;
-        this.users = users;
+    private String identity; // Mapped to clients identity
+    private ArrayList<Message> messages; // Array to store messages
+    private ArrayList<String> subscribers; //
+    private ArrayList<Server.ClientThread> subscribedThreads; // Threads of clients subscribed to channel
+    public Channel(String identity, ArrayList<String> subscribers) {
+        this.identity = identity;
+        this.subscribers = subscribers;
         messages = new ArrayList<>();
+        this.subscribedThreads = new ArrayList<>();
     }
-
+    public Channel(String identity) {
+        this.identity = identity;
+        this.subscribers = new ArrayList<>();;
+        messages = new ArrayList<>();
+        this.subscribedThreads = new ArrayList<>();
+    }
+    public void addSubscriber(String identity){
+        subscribers.add(identity);
+    }
+    public void removeSubscriber(String identity){
+        subscribers.remove(identity);
+    }
+    public void addSubscriberThread(Server.ClientThread thread){
+        if(!subscribedThreads.contains(thread)){
+            subscribedThreads.add(thread);
+        }
+    }
+    public void unsubscribeThread(Server.ClientThread thread){
+        if(subscribedThreads.contains(thread)){
+            subscribedThreads.remove(thread);
+        }
+    }
     public ArrayList<Message> getMessages() {
         return messages;
     }
 
-    public void setMessages(ArrayList<Message> messages) {
-        this.messages = messages;
+    public String getIdentity() {
+        return identity;
     }
 
-    public String getChannelId() {
-        return channelId;
+    public void sendMessageToSubscribers(Message message){
+        JSONObject response = message.toJSONObject();
+        for (Server.ClientThread thread : this.subscribedThreads) {
+            thread.sendResponse(response);
+        }
     }
-
-    public void setChannelId(String channelId) {
-        this.channelId = channelId;
-    }
-
-    public String getChannelName() {
-        return channelName;
-    }
-
-    public void setChannelName(String channelName) {
-        this.channelName = channelName;
-    }
-
-    public ArrayList<String> getUsers() {
-        return users;
-    }
-
-    public void setUsers(ArrayList<String> users) {
-        this.users = users;
-    }
-    
 }
